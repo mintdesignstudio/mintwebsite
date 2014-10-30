2f157d2fee1aeb18709d16e40e2829268df6b19e
Thomas Viktil <mandarin@mandarin.no>
2014-10-30 10:49:48 +0100

    Formatting

--- main.js
+++ main.js
@@ -7,19 +7,26 @@ var web       = require('./app/web');
 process.on('SIGTERM', onShutdown);
 
 logger.log({
-    type:           'info',
-    msg:            'starting server'
+    type: 'info',
+    msg:  'starting server'
 });
 
 var server = http.createServer(web());
 server.listen(config.port, onListen);
 
 function onListen() {
-    logger.log({ type: 'info', msg: 'listening', port: server.address().port });
+    logger.log({
+        type: 'info',
+        msg: 'listening',
+        port: server.address().port
+    });
 }
 
 function onShutdown() {
-    logger.log({ type: 'info', msg: 'shutting down' });
+    logger.log({
+        type: 'info',
+        msg: 'shutting down'
+    });
     server.close(function() {
         logger.log({ type: 'info', msg: 'exiting' });
         process.exit();
