8aeb64eb6ae40d35e4cd2cef234f23d927a818f5
Thomas Viktil <mandarin@mandarin.no>
2014-11-09 14:35:27 +0100

    Testing repaint issues i Chrome

--- app/views/js/loadcss.js
+++ app/views/js/loadcss.js
@@ -1,11 +1,14 @@
 var cb = function() {
-    var l = document.createElement('link'); l.rel = 'stylesheet';
+    var l = document.createElement('link');
+    l.rel = 'stylesheet';
     l.href = '/public/style.css';
     var h = document.getElementsByTagName('head')[0];
-    h.parentNode.insertBefore(l, h);
+    h.appendChild(l);
 };
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-          webkitRequestAnimationFrame || msRequestAnimationFrame;
+var raf = window.requestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.msRequestAnimationFrame;
 if (raf) {
     raf(cb);
 } else {
