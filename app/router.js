b6a343748da812302520b4619a39ee8a688a21ab
Thomas Viktil <mandarin@mandarin.no>
2014-12-02 21:33:06 +0100

    Cleaning up

--- app/router.js
+++ app/router.js
@@ -8,29 +8,37 @@ var middleware      = require('./middleware');
 
 var threeMonths = 60 * 60 * 24 * 90;
 
-middleware.is_under_construction(true);
-
 module.exports = function() {
     var router = express.Router();
 
     router
-        .get('/',               middleware.prismic, middleware.construction,
-                                    app.home)
+        .get('/',
+             middleware.prismic,
+             middleware.construction,
+             app.home)
 
-        .get('/about',          middleware.prismic, middleware.construction,
-                                    app.about)
+        .get('/about',
+             middleware.prismic,
+             middleware.construction,
+             app.about)
 
-        .get('/contact',        middleware.prismic, middleware.construction,
-                                    app.contact)
+        .get('/contact',
+             middleware.prismic,
+             middleware.construction,
+             app.contact)
 
-        .get('/works',          middleware.prismic, middleware.construction,
-                                    app.projects)
+        .get('/works',
+             middleware.prismic,
+             middleware.construction,
+             app.projects)
 
-        .get('/work/:name',     middleware.prismic, middleware.construction,
-                                    app.project)
+        .get('/work/:name',
+             middleware.prismic,
+             middleware.construction,
+             app.project)
 
-        .use('/public',         express.static(config.public_dir,
-                                    { maxAge: threeMonths }));
+        .use('/public',
+             express.static(config.public_dir, { maxAge: threeMonths }));
 
     if (config.development) {
         router.get('/app/views/:folder/:file', function(req, res, next) {
