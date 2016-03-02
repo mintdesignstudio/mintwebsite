f695207a3f76b89f5069157e1a9e55d2c43b1b1c
Thomas Viktil <mandarin@mandarin.no>
2016-03-02 22:12:22 +0100

    Don't need prod dir

--- production/public/js/loadcss.js
+++
@@ -1,1 +0,0 @@
-var cb=function(){var e=document.createElement("link");e.rel="stylesheet",e.href="/public/css/style.css";var t=document.getElementsByTagName("head")[0];t.appendChild(e)};window.addEventListener("DOMContentLoaded",cb);
