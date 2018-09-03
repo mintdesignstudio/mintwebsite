d98f55b91747621ba35e817da3d80bf3f2ca7086
mandarinx <mandarin@mandarin.no>
2018-09-03 22:48:49 +0200

    Updated preprod with recent changes in modules

--- preprod.js
+++ preprod.js
@@ -3,30 +3,15 @@ var path            = require('path');
 var fse             = require('fs-extra');
 var klawSync        = require('klaw-sync');
 var morecss         = require('more-css');
-var inlineSource    = require('inline-source');
-var Imagemin        = require('imagemin');
 var Minimize        = require('minimize');
 var UglifyJS        = require('uglify-js');
 var cheerio         = require('cheerio');
 var sri             = require('node-sri');
 var Promise         = require('promise');
-
-var svgo = Imagemin.svgo([
-    {removeViewBox: true},
-    {removeEmptyAttrs: true},
-    {removeEmptyContainers: true},
-    {removeEmptyText: true},
-    {removeHiddenElems: true},
-    {removeMetadata: true},
-    {removeRasterImages: true},
-    {removeUselessStrokeAndFill: true},
-    {cleanupAttrs: true},
-    {removeComments: true},
-    {removeDesc: true},
-    {removeDoctype: true}
-]);
-
-var optipng = Imagemin.optipng({ optimizationLevel: 3 });
+const { inlineSource }  = require('inline-source');
+const imagemin          = require('imagemin');
+const imageminPngquant  = require('imagemin-pngquant');
+const imageminSvgo      = require('imagemin-svgo');
 
 var dir = {
     prod:           __dirname + '/production/',
@@ -40,18 +25,18 @@ var dir = {
 
 // -----------------------------------------------------------------------------
 
-cleanCopyDir(dir.pub,       dir.prodpub, function() {
+cleanCopyDir(dir.pub, dir.prodpub, function() {
     console.log('Copy '+dir.prodpub);
 
-    cleanCopyDir(dir.appview,   dir.prodappview, function() {
+    cleanCopyDir(dir.appview, dir.prodappview, function() {
         console.log('Copy '+dir.prodappview);
 
         minifyCss(dir.prod);
         uglify(dir.prodpub + 'js/');
         sriHash(dir.prodappview + 'layouts/', '.hbs');
-        inline(dir.prodappview + 'layouts/', '.hbs');
-        imagemin(dir.prodpub + 'images/', '.svg', svgo);
-        imagemin(dir.prodpub + 'images/', '.png', optipng);
+        inlineAll();
+        svgmin();
+        pngmin();
         htmlmin(dir.prodappview, '.hbs');
     });
 });
@@ -165,42 +150,58 @@ function htmlmin(dir, ext) {
     console.log('');
 }
 
-function imagemin(dir, ext, minifier) {
-    console.log('Imagemin '+dir+'*'+ext);
-
-    var imagemin = new Imagemin()
-        .src(dir + '*' + ext)
-        .dest(dir)
-        .use(minifier);
-
-    imagemin.run(function(err, files) {
-        if (err) {
-            throw err;
-        }
-    });
-
-    console.log('');
+async function svgmin() {
+    await imagemin([dir.prodpub + 'images/*.svg'],
+             dir.prodpub + 'images',
+             { use: [
+                imageminSvgo({
+                    plugins: [
+                        {removeViewBox: true},
+                        {removeEmptyAttrs: true},
+                        {removeEmptyContainers: true},
+                        {removeEmptyText: true},
+                        {removeHiddenElems: true},
+                        {removeMetadata: true},
+                        {removeRasterImages: true},
+                        {removeUselessStrokeAndFill: true},
+                        {cleanupAttrs: true},
+                        {removeComments: true},
+                        {removeDesc: true},
+                        {removeDoctype: true}
+                    ]
+                })
+            ]});
+    console.log('SVGs optimized');
 }
 
-function inline(dir, ext) {
-    console.log('Inline');
+async function pngmin() {
+    await imagemin([dir.prodpub + 'images/*.png'],
+         dir.prodpub + 'images/',
+         {use: [imageminPngquant({
+            quality: '80',
+         })]})
+    console.log('PNGs optimized');
+}
 
-    var files = listFiles(dir, ext)
-        .map(function(file) {
-            console.log('    '+file.path);
+function inlineAll() {
+    let files = listFiles(dir.prodappview + 'layouts/', '.hbs')
+        .map((file) => {
             return file.path;
         });
 
-    files.forEach(function(file) {
-        var html = inlineSource(file, {
-            compress: false,
-            swallowErrors: false,
-            rootpath: path.resolve('production/')
-        });
-        fs.writeFileSync(file, html);
+    files.forEach((file) => {
+        inline(file);
     });
+    console.log('Inlined sources');
+}
 
-    console.log('');
+async function inline(file) {
+    let html = await inlineSource(file, {
+        compress: false,
+        swallowErrors: false,
+        rootpath: path.resolve('production/')
+    });
+    fs.writeFileSync(file, html);
 }
 
 function minifyCss(dir) {
