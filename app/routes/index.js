830a93c001a53b22d13982261b5859811ce6d66e
mandarinx <mandarin@mandarin.no>
2018-09-08 12:29:45 +0200

    temporarily redirect when in dev

--- app/routes/index.js
+++ app/routes/index.js
@@ -17,30 +17,30 @@ const sitemap      = require('./sitemap');
 
 module.exports = function(app) {
 
-    if (config.production) {
+    if (!config.production) {
         // redirect to https and non-www
         app.get('*', function(req,res,next) {
             let host = req.header('host');
 
-logger.info('host: ' + host);
-logger.info('x-forwarder-proto: ' + req.headers['x-forwarded-proto']);
-logger.info('match www: ' + host.match(/^www\..*/i));
+            logger.info('host: ' + host);
+            logger.info('x-forwarder-proto: ' + req.headers['x-forwarded-proto']);
+            logger.info('match www: ' + host.match(/^www\..*/i));
 
-            console.log('host:', host);
-            console.log('x-forwarder-proto:', req.headers['x-forwarded-proto']);
-            console.log('match www:', host.match(/^www\..*/i));
+            // console.log('host:', host);
+            // console.log('x-forwarder-proto:', req.headers['x-forwarded-proto']);
+            // console.log('match www:', host.match(/^www\..*/i));
 
             if (req.headers['x-forwarded-proto'] === 'https' &&
                 !host.match(/^www\..*/i)) {
 
-logger.info('no redirect necessary');
-                console.log('no redirect necessary');
+                logger.info('no redirect necessary');
+                // console.log('no redirect necessary');
                 next();
                 return;
             }
 
-logger.log('redirect to: https://' + host.substr(4) + req.url);
-            console.log('redirect to', 'https://' + host.substr(4) + req.url);
+            logger.log('redirect to: https://' + host.substr(4) + req.url);
+            // console.log('redirect to', 'https://' + host.substr(4) + req.url);
             res.redirect(301, 'https://' + host.substr(4) + req.url);
         });
     }
